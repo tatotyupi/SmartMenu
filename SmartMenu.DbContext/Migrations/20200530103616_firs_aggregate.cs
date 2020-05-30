f4b90d83fcd6d47de9f719f7a4d87731695b7c41
tyupi medzmariashvili <tato.medzmariashvili@gmail.com>
2020-05-30 14:36:34 +0400

    added firs migration

---
+++ SmartMenu.DbContext/Migrations/20200530103616_firs_aggregate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartMenu.DbContext.Migrations
+{
+    public partial class firs_aggregate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aggregates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntityKey = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aggregates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aggregates");
+        }
+    }
+}
--- SmartMenu.Web.Api/Startup.cs
+++ SmartMenu.Web.Api/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SmartMenu.DbContext;
+using SmartMenu.DbContext.Infrastructure;
 
 namespace SmartMenu.Web.Api
 {
@@ -26,6 +29,9 @@ namespace SmartMenu.Web.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services
+               .AddDbContext<SmartMenuDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SmartMenuDbContext")))
+               .AddSingleton(new SmartMenuDbContextFactory(Configuration.GetConnectionString("SmartMenuDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
